55b594e8778c38bdf20e419aa9b8441bb20d609d
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-11-18 15:26:18 +0000

    Updated url

--- site.config.ts
+++ site.config.ts
@@ -5,8 +5,8 @@ const siteConfig = {
   blogDatabaseId: "83e70927-9083-4a1c-af7a-f690b371768c",
   eventsDatabaseId: "7c5bc493-7a66-404e-9f10-1b93edc91e7a",
   commmittee: committee as CommitteeMember[],
-  fundingUrl: "https://youtu.be/dQw4w9WgXcQ",
-  primarySiteUrl: "https://emgineer.vercel.app",
+  fundingUrl: "https://www.vle.cam.ac.uk/mod/resource/view.php?id=19037905",
+  primarySiteUrl: "https://emmaengsoc.com",
   admins: ["ls914", "basiltfake", "th621", "jm2455"],
 };
 
